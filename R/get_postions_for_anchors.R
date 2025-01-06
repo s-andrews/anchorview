fe6fb5577aeda4150b0d4675989277db1fe30154
s-andrews <simon@proteo.me.uk>
2025-01-06 09:55:08 +0000

    Fix position extraction and update vignette

--- R/get_postions_for_anchors.R
+++ R/get_postions_for_anchors.R
@@ -22,25 +22,14 @@ get_positions_for_anchors <- function(anchors, min_score=0, reduction="umap") {
     dplyr::group_by(dataset1,dataset2) |>
     dplyr::mutate(cellid1 = colnames(anchors@object.list[[dataset1[1]]])[cell1]) |>
     dplyr::mutate(cellid2 = colnames(anchors@object.list[[dataset2[1]]])[cell2]) |>
+    dplyr::mutate(cellid1_x = Seurat::Embeddings(anchors@object.list[[dataset1[1]]], reduction = reduction)[cellid1,] |> tibble::as_tibble() |> dplyr::pull(1) )|>
+    dplyr::mutate(cellid1_y = Seurat::Embeddings(anchors@object.list[[dataset1[1]]], reduction = reduction)[cellid1,] |> tibble::as_tibble() |> dplyr::pull(2) )|>
+    dplyr::mutate(cellid2_x = Seurat::Embeddings(anchors@object.list[[dataset2[1]]], reduction = reduction)[cellid2,] |> tibble::as_tibble() |> dplyr::pull(1) )|>
+    dplyr::mutate(cellid2_y = Seurat::Embeddings(anchors@object.list[[dataset2[1]]], reduction = reduction)[cellid2,] |> tibble::as_tibble() |> dplyr::pull(2) )|>
     dplyr::ungroup() -> positions
 
 
-
-  Seurat::Embeddings(anchors@object.list[[anchors@anchors$dataset1[1]]], reduction = reduction) |>
-    tibble::as_tibble(rownames="cellid1") |>
-    dplyr::select(1:3) -> cellid1_positions
-
-  colnames(cellid1_positions) <- c("cellid1","cellid1_x", "cellid1_y")
-
-  Seurat::Embeddings(default_anchors@object.list[[default_anchors@anchors$dataset2[1]]], reduction = reduction) |>
-    tibble::as_tibble(rownames="cellid2") |>
-    dplyr::select(1:3) -> cellid2_positions
-
-  colnames(cellid2_positions) <- c("cellid2","cellid2_x", "cellid2_y")
-
   positions |>
-    dplyr::left_join(cellid1_positions) |>
-    dplyr::left_join(cellid2_positions) |>
     tibble::add_column(anchor_weight=1) |>
     tibble::as_tibble() -> positions
 
